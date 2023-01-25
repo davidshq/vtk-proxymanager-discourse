82f86de74774db85e54a83c216244fa68dfd7149
Dave Mackey <dave@davemackey.net>
2023-01-25 14:13:51 +0000

    proxy and proxymanager

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
+// See: https://github.com/Kitware/VolView/blob/main/src/vtk/proxy.js
 import '@kitware/vtk.js/Rendering/Profiles/Geometry';
 
+import vtkProxyManager from '@kitware/vtk.js/Proxy/Core/ProxyManager';
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
-
-import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
 import vtkConeSource from '@kitware/vtk.js/Filters/Sources/ConeSource';
+import proxyConfiguration from './proxyConfiguration';
 
+// Render window
 const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
 const renderer = fullScreenRenderer.getRenderer();
 const renderWindow = fullScreenRenderer.getRenderWindow();
 
-// Create our cone
+// Create cone
 const coneSource = vtkConeSource.newInstance({ height: 1.0 });
 
-const mapper = vtkMapper.newInstance();
-mapper.setInputConnection(coneSource.getOutputPort());
+// Create and initialize our ProxyManager
+const proxyManager = vtkProxyManager.newInstance({ proxyConfiguration });
 
-const actor = vtkActor.newInstance();
-actor.setMapper(mapper);
+// Create sourceProxy
+let sourceProxy = proxyManager.createProxy('Sources', 'TrivialProducer');
+sourceProxy.setInputData(coneSource);
 
-renderer.addActor(actor);
-renderer.resetCamera();
-renderWindow.render();
+// Create our ViewProxy
+const viewProxy = proxyManager.createProxy('Views', 'View3D');
+
+// Use a new DOM element as container for the ViewProxy
+const container = document.createElement('div');
+
+document.body.appendChild(container);
+viewProxy.setContainer(container);
+
+const rep = proxyManager.getRepresentation(sourceProxy, viewProxy);
+
+let views = proxyManager.getViews();
+let view = null;
+view = views[0];
+view.getRepresentation();
+view.render(true);
