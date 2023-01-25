3d26dbeac96a8c55f719f6102a1d9b04951c504c
Dave Mackey <dave@davemackey.net>
2023-01-25 15:04:10 +0000

    Add post sample code

--- src/main.js
+++ src/main.js
@@ -5,3 +5,10 @@ import vtkProxyManager from '@kitware/vtk.js/Proxy/Core/ProxyManager';
 import proxyConfig from 'proxyConfig';
 
 const proxyManager = vtkProxyManager.newInstance({ proxyConfig });
+
+source = proxyManager.createProxy('Sources', 'TrivialProducer', { name: 'myImage' });
+source.setInputData(myImageData);
+view = proxyManager.createProxy('Views', 'View3D', { name: 'my3DView' });
+view.setContainer(someHTMLElement);
+rep = proxyManager.getRepresentation(source, view);
+view.addRepresentation(rep);
